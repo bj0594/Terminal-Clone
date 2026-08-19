85f098e9505d19252cd508db30affccc74809463
rmk062026 <rmk062026@gmail.com>
2026-08-19 10:26:06 +0200

    Added structure in LineManager and test code in Program.cs

---
+++ LineManager.cs
@@ -0,0 +1,17 @@
+namespace Terminal_Clone;
+
+public class LineManager
+{
+    public void Head()
+    {
+
+    }
+    public void Tail()
+    {
+
+    }
+    public void Wc()
+    {
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        while (true)
+        {
+            Console.Write("> ");
+            string? input = Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                continue;
+            }
+
+            if (input.ToLower() == "exit")
+            {
+                break;
+            }
+            Console.WriteLine($"You entered: {input}");
+        }
     }
 }
