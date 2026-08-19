f1e492600193b56cc051f47c24b9f2be956fc187
bj0594 <bj0594.kode@gmail.com>
2026-08-19 11:19:36 +0200

    added method for copying file, with and without testfolder

--- FileManager.cs
+++ FileManager.cs
@@ -1,30 +1,50 @@
+using System.IO;
+
 public static class Commands
 {
     // Creates a new empty file using the filename provided by the user.
     public static void Touch(string[] parts)
     {
-        // ...
+        string filename = parts[1];
+        File.Create(filename).Dispose();
     }
 
 
     // Copies a file from the source path to the destination path.
     public static void Copy(string[] parts)
     {
-        // ...
+        string source = parts[1];
+        string destination = parts[2];
+
+        if(Directory.Exists(destination))
+        {
+            File.Copy(
+                source,
+                Path.Combine(
+                    destination, 
+                    Path.GetFileName(source)
+                )
+            );
+        }
+        else
+        {
+            File.Copy(source, destination);
+        }
     }
 
 
     // Moves a file to a new location or renames it.
     public static void Move(string[] parts)
     {
-        // ...
+        
     }
 
 
     // Deletes a file from the specified path.
     public static void Remove(string[] parts)
     {
-        // ...
+        string filename = parts[1];
+        File.Delete(filename);
     }
 
 
